87d8cd2c19979f8e594452db27f7a674f6248eb3
oizy404 <71779024+oizy404@users.noreply.github.com>
2021-05-15 21:21:02 +0800

    online payment transaction and engagement system

--- public/js/admin-sidenav.js
+++ public/js/admin-sidenav.js
@@ -1,12 +1,5 @@
-$("#btn-sidenav").click(function(){
-    let value = $(this).html();
-    $(".side-nav").toggle(300);
-});
-
-
 $(document).ready(function(){
   $('.nav_btn').click(function(){
     $('.mobile_nav_items').toggleClass('active');
   });
-
 });
