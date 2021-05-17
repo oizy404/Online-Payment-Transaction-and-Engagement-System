5281978ea0d97bf5fec32e560d81a4c8211992af
oizy404 <71779024+oizy404@users.noreply.github.com>
2021-05-17 22:38:10 +0800

    online payment transaction and engagement system

--- resources/views/pages/show-msg-assoc.blade.php
+++ resources/views/pages/show-msg-assoc.blade.php
@@ -8,6 +8,6 @@
     <div class="container">
         <p>{{$message_associate->associate_id}}</p>
         <p>{{$message_associate->subject}}</p>
-        <img src="{{asset('images')}}/{{$message_associate->msg_imagefile}}" alt="image msg" style="max-width:60px;">
+        <img src="{{asset('img_msgassociates')}}/{{$message_associate->msg_imagefile}}" alt="image msg" style="max-width:500px;">
     </div>
 @stop
