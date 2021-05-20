96e62b49a44502962b7434699f9611e865c7d32d
genelyn <genelynford@gmail.com>
2021-05-20 21:28:15 +0800

    online payment transaction and engagement system

--- resources/views/pages/admin-home.blade.php
+++ resources/views/pages/admin-home.blade.php
@@ -6,61 +6,67 @@
 
 @section('content')
 @include('shared.admin-header')
-@include('shared.admin-sidenav')   
-<div class="admin-dashboard">
-    <div class="admindash-container">
-        <div class="row mt-5" id="admin-dashcards">
-            <div class="col-md-3 offset-md-3" id="card-users">
-                <div class="card cards">
-                    <center>
-                    <div class="card-body">
-                        <i class="fas fa-users rounded-circle"></i>
-                        <h5 class="card-title mt-2">Bulano's Associates Accounts</h5>
-                    </div>
-                    </center>
-                    <div class="card-footer">
-                        More Info...<i class="fas fa-arrow-right" style="float: right;"></i>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-3 offset-md-1" id="card-users">
-                <div class="card" id="admindash-card">
-                    <center>
-                    <div class="card-body">
-                        <i class="fas fa-users rounded-circle"></i>
-                        <h5 class="card-title mt-2">Bulano's Clients Accounts</h5>
-                    </div>
-                    </center>
-                    <div class="card-footer">
-                        More Info...<i class="fas fa-arrow-right" style="float: right;"></i>
+@include('shared.admin-sidenav') 
+ 
+<div class="content">
+    <div class="admin-dashboard">
+        <div class="admindash-container">
+            <div class="row mt-5" id="admin-dashcards">
+                <div class="col-md-3 offset-md-3" id="card-users">
+                    <div class="card">
+                        <center>
+                        <div class="card-body">
+                            <i class="fas fa-users rounded-circle"></i>
+                            <h5 class="card-title mt-2">Bulano's Associates Accounts</h5>
+                        </div>
+                        </center>
+                        <div class="card-footer">
+                            More Info...<i class="fas fa-arrow-right" style="float: right;"></i>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-md-3 offset-md-3" id="cardcreate-account">
-                <div class="card" id="card-create">
-                    <center>
-                    <div class="card-body">
-                        <i class="fas fa-user-plus rounded-circle"></i>
-                        <h5 class="card-title mt-2">Create Associate Account</h5>
-                    </div>
-                    </center>
-                    <div class="card-footer card-assoc">
-                        Create<i class="fas fa-arrow-right" style="float: right;"></i>
+                <div class="col-md-3 offset-md-1" id="card-users">
+                    <div class="card" id="admindash-card">
+                        <div class="card">
+                            <center>
+                            <div class="card-body">
+                                <i class="fas fa-users rounded-circle"></i>
+                                <h5 class="card-title mt-2">Bulano's Clients Accounts</h5>
+                            </div>
+                            </center>
+                            <div class="card-footer">
+                                More Info...<i class="fas fa-arrow-right" style="float: right;"></i>
+                            </div>
+                        </div>    
                     </div>
                 </div>
-            </div>
-            <div class="col-md-3 offset-md-1" id="cardcreate-account">
-                <div class="card">
-                    <center>
-                    <div class="card-body">
-                        <i class="fas fa-user-plus rounded-circle"></i>
-                        <h5 class="card-title mt-2">Create Client Account</h5>
+                <div class="col-md-3 offset-md-3" id="cardcreate-account">
+                    <div class="card" id="card-create">
+                        <div class="card">
+                            <center>
+                            <div class="card-body">
+                                <i class="fas fa-user-plus rounded-circle"></i>
+                                <h5 class="card-title mt-2">Create Associate Account</h5>
+                            </div>
+                            </center>
+                            <div class="card-footer card-assoc">
+                                Create<i class="fas fa-arrow-right" style="float: right;"></i>
+                            </div>
+                        </div>      
                     </div>
-                    </center>
-                    <div class="card-footer card-clients">
-                        Create<i class="fas fa-arrow-right" style="float: right;"></i>
+                </div>
+                <div class="col-md-3 offset-md-1" id="cardcreate-account">
+                    <div class="card">
+                        <center>
+                        <div class="card-body">
+                            <i class="fas fa-user-plus rounded-circle"></i>
+                            <h5 class="card-title mt-2">Create Client Account</h5>
+                        </div>
+                        </center>
+                        <div class="card-footer card-clients">
+                            Create<i class="fas fa-arrow-right" style="float: right;"></i>
+                        </div>
                     </div>
-                    <p></p>
                 </div>
             </div>
         </div>
